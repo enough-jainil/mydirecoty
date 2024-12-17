751b9891eeea13be1ac17d842d80087ab6af7698
Jainil Prajapati 🪐 <86187588+enough-jainil@users.noreply.github.com>
2024-12-17 19:16:21 +0530

    Initialize add-hero-section-simple-left

---
+++ app.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+
+const colorMode = useColorMode();
+
+const background = computed(() => colorMode.value === "dark" ? "bg-gray-800" : "bg-white");
+
+const { layout } = useContent();
+
+const layoutName = computed(() => layout.value || 'default');
+</script>
+
+<template>
+
+  <Body :class="background">
+    <AppLayout>
+      <NuxtLayout :name="layoutName">
+        <NuxtPage class="page-enter-active" />
+      </NuxtLayout>
+    </AppLayout>
+  </Body>
+</template>
+
+<style>
+@keyframes fadeInUp {
+  from {
+    opacity: 0;
+    transform: translateY(.5rem)
+  }
+
+  to {
+    opacity: 1;
+    transform: none
+  }
+}
+
+@keyframes fadeOutDown {
+  from {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+    transform: translateY(.5rem)
+  }
+}
+
+.page-enter-active {
+  animation: fadeInUp .6s forwards
+}
+
+.page-leave-active {
+  animation: fadeOutDown .6s backwards;
+}
+</style>
